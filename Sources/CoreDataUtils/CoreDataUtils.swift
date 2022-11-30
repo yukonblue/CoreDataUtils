10cc85065841628b4e0f571471528a9583c14ee9
yukonblue <77284557+yukonblue@users.noreply.github.com>
2022-11-30 08:28:35 -0500

    Establish `CoreDataUtils` Swift package

---
+++ Sources/CoreDataUtils/CoreDataUtils.swift
@@ -0,0 +1,6 @@
+public struct CoreDataUtils {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CoreDataUtilsTests/CoreDataUtilsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CoreDataUtils
+
+final class CoreDataUtilsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CoreDataUtils().text, "Hello, World!")
+    }
+}
